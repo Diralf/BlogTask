29c0d05d57b8a6043baff8e65bc2a04f4a5f1584
diralf <diralfeastarea@gmail.com>
2014-12-19 01:06:07 +0300

    dusty blog

---
+++ script.js
@@ -0,0 +1,245 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+post = [
+    {
+        title: "Super Meat Boy",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/40800/header.jpg?t=1386730867",
+        genre: "Indie",
+        developer: "Team Meat",
+        release: "30 Nov, 2010",
+        tags: ["Indie", "Platform", "2D", "Difficult"],
+        description: "Super Meat Boy is a tough as nails platformer where you play as an animated cube of meat who's trying to save his girlfriend (who happens to be made of bandages) from an evil fetus in a jar wearing a tux.                        Our meaty hero will leap from walls, over seas of buzz saws, through crumbling caves and pools of old needles. Sacrificing his own well being to save his damsel in distress. Super Meat Boy brings the old school difficulty of classic NES titles like Mega Man 2, Ghost and Goblins and Super Mario Bros. 2 (The Japanese one) and stream lines them down to the essential no BS straight forward twitch reflex platforming.                  Ramping up in difficulty from hard to soul crushing SMB will drag Meat boy though haunted hospitals, salt factories and even hell itself. And if 300+ single player levels weren't enough SMB also throws in epic boss fights, a level editor and tons of unlock able secrets, warp zones and hidden characters. ",
+        screenshots: [
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_2482dad154fa38c32195a5301891ec7d9cefa7da.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_652092068cc0d5e75fe3f199ad1bbc395806c1f2.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_71f114e451550394500613d8ec8082ae3eac44e5.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_d158c2843c714bf2294376e03d9c4d6c7cd96397.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_8579adfaa58c863497e71df1b36331d37e51aeab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_84f8832c8cfb5d93dba79c099b0067e22447cbab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_1d69a008fd9716dd2c35f8c1b5ccd467cd1a2ad3.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_b2998adf37b0276c240315732169e06886942e78.1920x1080.jpg?t=1386730867"}
+        ],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "Braid",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/26800/header.jpg?t=1413585054",
+        genre: "Casual, Indie, Strategy",
+        developer: "Jonathan Blow",
+        release: "10 Apr, 2009",
+        tags: ["Indie", "Platform", "2D"],
+        description: "Braid is a puzzle-platformer, drawn in a painterly style, where you can manipulate the flow of time in strange and unusual ways. From a house in the city, journey to a series of worlds and solve puzzles to rescue an abducted princess.",
+        screenshots: [
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_2482dad154fa38c32195a5301891ec7d9cefa7da.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_652092068cc0d5e75fe3f199ad1bbc395806c1f2.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_71f114e451550394500613d8ec8082ae3eac44e5.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_d158c2843c714bf2294376e03d9c4d6c7cd96397.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_8579adfaa58c863497e71df1b36331d37e51aeab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_84f8832c8cfb5d93dba79c099b0067e22447cbab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_1d69a008fd9716dd2c35f8c1b5ccd467cd1a2ad3.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_b2998adf37b0276c240315732169e06886942e78.1920x1080.jpg?t=1386730867"}
+        ],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "FEZ",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/224760/header.jpg?t=1383936243",
+        genre: "Indie",
+        developer: "Polytron Corporation",
+        release: "1 May, 2013",
+        tags: ["Indie", "Platform", "Puzzle", "Exploration", "Adventure"],
+        description: "Gomez is a 2D creature living in a 2D world. Or is he? When the existence of a mysterious 3rd dimension is revealed to him, Gomez is sent out on a journey that will take him to the very end of time and space. Use your ability to navigate 3D structures from 4 distinct classic 2D perspectives.",
+        screenshots: [
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_56dbc476fb535da688e126dde9402ff682bc5874.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_acfee8996e8e1e13c3a4db9a784ee1e297850537.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_e7739ce8acb1fa3f4f9acb08a938803c5b40ffe8.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_4394c6e179fa87e8f8231880bf0e166da1dd8768.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_3ca0c443b1c7e7b76da98daaf2065ab1f982d7ae.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_bf7e69d788e8d0e8bd6097f8d2f515f4fb4e1756.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_0b27910818a8f7616fd491fee8e5855846e3f368.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_9926a8bea531bde994dc96b15c11cb7667021daf.1920x1080.jpg?t=1383936243"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/224760/ss_80664cc7cdd064bb31ef57b1471df225ef244b79.1920x1080.jpg?t=1383936243"}
+        ],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "Braid2",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/26800/header.jpg?t=1413585054",
+        genre: "Casual, Indie, Strategy",
+        developer: "Jonathan Blow",
+        release: "10 Apr, 2009",
+        tags: ["Indie", "Platform", "2D"],
+        description: "Braid is a puzzle-platformer, drawn in a painterly style, where you can manipulate the flow of time in strange and unusual ways. From a house in the city, journey to a series of worlds and solve puzzles to rescue an abducted princess.",
+        screenshots: [
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_2482dad154fa38c32195a5301891ec7d9cefa7da.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_652092068cc0d5e75fe3f199ad1bbc395806c1f2.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_71f114e451550394500613d8ec8082ae3eac44e5.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_d158c2843c714bf2294376e03d9c4d6c7cd96397.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_8579adfaa58c863497e71df1b36331d37e51aeab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_84f8832c8cfb5d93dba79c099b0067e22447cbab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_1d69a008fd9716dd2c35f8c1b5ccd467cd1a2ad3.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_b2998adf37b0276c240315732169e06886942e78.1920x1080.jpg?t=1386730867"}
+        ],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "Braid2",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/26800/header.jpg?t=1413585054",
+        genre: "Casual, Indie, Strategy",
+        developer: "Jonathan Blow",
+        release: "10 Apr, 2009",
+        tags: ["Indie", "Platform", "2D"],
+        description: "Braid is a puzzle-platformer, drawn in a painterly style, where you can manipulate the flow of time in strange and unusual ways. From a house in the city, journey to a series of worlds and solve puzzles to rescue an abducted princess.",
+        screenshots: [
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_2482dad154fa38c32195a5301891ec7d9cefa7da.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_652092068cc0d5e75fe3f199ad1bbc395806c1f2.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_71f114e451550394500613d8ec8082ae3eac44e5.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_d158c2843c714bf2294376e03d9c4d6c7cd96397.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_8579adfaa58c863497e71df1b36331d37e51aeab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_84f8832c8cfb5d93dba79c099b0067e22447cbab.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_1d69a008fd9716dd2c35f8c1b5ccd467cd1a2ad3.1920x1080.jpg?t=1386730867"},
+            {img: "http://cdn.akamai.steamstatic.com/steam/apps/40800/ss_b2998adf37b0276c240315732169e06886942e78.1920x1080.jpg?t=1386730867"}
+        ],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "Braid2",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/26800/header.jpg?t=1413585054",
+        genre: "Casual, Indie, Strategy",
+        developer: "Jonathan Blow",
+        release: "10 Apr, 2009",
+        tags: ["Indie", "Platform", "2D"],
+        description: "Braid is a puzzle-platformer, drawn in a painterly style, where you can manipulate the flow of time in strange and unusual ways. From a house in the city, journey to a series of worlds and solve puzzles to rescue an abducted princess.",
+        screenshots: ["http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007886.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007887.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007885.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007898.1920x1080.jpg?t=1413585054"],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }, {
+        title: "Braid2",
+        image: "http://cdn.akamai.steamstatic.com/steam/apps/26800/header.jpg?t=1413585054",
+        genre: "Casual, Indie, Strategy",
+        developer: "Jonathan Blow",
+        release: "10 Apr, 2009",
+        tags: ["Indie", "Platform", "2D"],
+        description: "Braid is a puzzle-platformer, drawn in a painterly style, where you can manipulate the flow of time in strange and unusual ways. From a house in the city, journey to a series of worlds and solve puzzles to rescue an abducted princess.",
+        screenshots: ["http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007886.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007887.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007885.1920x1080.jpg?t=1413585054",
+            "http://cdn.akamai.steamstatic.com/steam/apps/26800/0000007898.1920x1080.jpg?t=1413585054"],
+        date: "01/12/2014",
+        author: "Journalkin"
+    }
+];
+
+var data = {
+    "full": false,
+    "post": []
+};
+
+var pageData = {
+    "page": 1,
+    "pages": [],
+    "maxPost": 3,
+    "first": false,
+    "last": false,
+    "prev": function () {
+        return function (text, render) {
+            return +render(text) - 1;
+        };
+    },
+    "next": function () {
+        return function (text, render) {
+            return +render(text) + 1;
+        };
+    }
+};
+
+var createContent = function (post) {
+    $("#page-post").load("templates.html #template-post-preview", function () {
+        var template = document.getElementById('template-post-preview').innerHTML;
+        pageData.page = getNumberPage();
+        var firstPost = (pageData.page - 1) * pageData.maxPost;
+        var lastPost = firstPost + pageData.maxPost;
+        for (var i = 0; i < post.length; i++) {
+            post[i].id = i;
+        }
+        data.post = [];
+        data.post = post.slice(firstPost, lastPost);
+        var output = Mustache.render(template, data);
+        $("#page-post").html(output);
+    });
+};
+
+var createPagination = function () {
+    $("#game-pagination-top").load("templates.html #template-pagination", function () {
+        var template = document.getElementById('template-pagination').innerHTML;
+        pageData.countPage = getCountPage();
+        for (var i = 0; i < pageData.countPage; i++) {
+            pageData.pages[i] = {number: i + 1, active: (i + 1) === pageData.page};
+        }
+        pageData.first = pageData.page === 1;
+        pageData.last = pageData.page === pageData.countPage;
+        var output = Mustache.render(template, pageData);
+        $("#game-pagination-top").html(output);
+        $("#game-pagination-bottom").html(output);
+    });
+};
+
+var openPost = function (id) {
+    $("#page-post").load("templates.html #template-post-preview", function () {
+        var template = document.getElementById('template-post-preview').innerHTML;
+        data.full = true;
+        data.post = [];
+        data.post[0] = post[id];
+        data.post[0].id = id;
+        var screens = data.post[0].screenshots;
+        screens[0].active = true;
+        for (var i = 0; i < screens.length; i++) {
+            screens[i].id = i;
+        }
+        var output = Mustache.render(template, data);
+        $("#page-post").html(output);
+        $('.carousel').carousel(0);
+    });
+};
+
+var getCountPage = function() {
+    return Math.ceil(post.length / pageData.maxPost);
+};
+
+var getNumberPage = function () {
+    var currentPage = +getUrlVars()["page"];
+    if (currentPage<1 || !currentPage || currentPage>getCountPage()) {
+        currentPage = 1;
+    }
+    return currentPage;
+};
+
+var initPage = function () {
+    createContent(post);
+    createPagination();
+};
+
+$(document).ready(function () {
+    initPage();
+});
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
